91cef5408a84406828e137a660f7ce5c98a1b546
Patrick von Steht <4501324-patrick-vonsteht@users.noreply.gitlab.com>
2024-07-21 14:59:57 +0200

    Add main program

--- src/main/java/io/github/patrick_vonsteht/PokerHandKata.java
+++ src/main/java/io/github/patrick_vonsteht/PokerHandKata.java
@@ -1,7 +1,50 @@
 package io.github.patrick_vonsteht;
 
 public final class PokerHandKata {
+    private static PokerJudge pokerJudge;
+
+    private static PokerHand hand1 = new PokerHand(
+            new Card(CardSuit.CLUBS, CardValue.TWO),
+            new Card(CardSuit.CLUBS, CardValue.TEN),
+            new Card(CardSuit.CLUBS, CardValue.JACK),
+            new Card(CardSuit.CLUBS, CardValue.FIVE),
+            new Card(CardSuit.HEARTS, CardValue.SIX)
+    );
+
+    private static PokerHand hand2 = new PokerHand(
+            new Card(CardSuit.DIAMONDS, CardValue.NINE),
+            new Card(CardSuit.CLUBS, CardValue.FOUR),
+            new Card(CardSuit.CLUBS, CardValue.JACK),
+            new Card(CardSuit.CLUBS, CardValue.FIVE),
+            new Card(CardSuit.CLUBS, CardValue.SIX)
+    );
+
     private PokerHandKata() {}
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        setup();
+        run();
+    }
+
+    private static void setup() {
+        final HighCardScorer highCardScorer = new HighCardScorer();
+        final PokerHandComparisonRule highCardRule = new HighCardRule(highCardScorer);
+        final FlushMatcher flushMatcher = new FlushMatcher();
+        final PokerHandComparisonRule flushRule = new FlushRule(flushMatcher, highCardRule);
+        pokerJudge = new PokerJudge(flushRule, highCardRule);
+    }
+
+    private static void run() {
+        final PokerJudgeResult result = pokerJudge.judge(hand1, hand2);
+
+        if (result.getType() == PokerJudgeResultType.WINNER) {
+            if (result.getWinner().equals(hand1)) {
+                System.out.println("Hand1 has won!");
+            } else {
+                System.out.println("Hand2 has won!");
+            }
+        } else {
+            System.out.println("Draw between hand1 and hand2!");
+        }
+    }
 }
