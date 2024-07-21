ed2e1fdeef9a111f220c85acc42b0d3837c4fd09
Patrick von Steht <4501324-patrick-vonsteht@users.noreply.gitlab.com>
2024-07-21 16:26:54 +0200

    Add four of a kind matcher

---
+++ src/test/java/io/github/patrick_vonsteht/FourOfAKindMatcherTest.java
@@ -0,0 +1,65 @@
+package io.github.patrick_vonsteht;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class FourOfAKindMatcherTest {
+
+    @Test
+    void FourOfAKindMatcherMatchesLowFourOfAKind() {
+        PokerHand hand = new PokerHand(
+                new Card(CardSuit.DIAMONDS, CardValue.TWO),
+                new Card(CardSuit.HEARTS, CardValue.FOUR),
+                new Card(CardSuit.SPADES, CardValue.TWO),
+                new Card(CardSuit.CLUBS, CardValue.TWO),
+                new Card(CardSuit.CLUBS, CardValue.TWO)
+        );
+
+        PokerHandMatcher matcher = new FourOfAKindMatcher();
+        assertTrue(matcher.matches(hand));
+    }
+
+    @Test
+    void ForOfAKindMatcherMatchesHighFourOfAKind() {
+        PokerHand hand = new PokerHand(
+                new Card(CardSuit.DIAMONDS, CardValue.FOUR),
+                new Card(CardSuit.HEARTS, CardValue.FOUR),
+                new Card(CardSuit.SPADES, CardValue.TWO),
+                new Card(CardSuit.CLUBS, CardValue.FOUR),
+                new Card(CardSuit.CLUBS, CardValue.FOUR)
+        );
+
+        PokerHandMatcher matcher = new FourOfAKindMatcher();
+        assertTrue(matcher.matches(hand));
+    }
+
+    @Test
+    void FourOfAKindMatcherDoesNotMatchNonFourOfAKind() {
+        PokerHand hand = new PokerHand(
+                new Card(CardSuit.DIAMONDS, CardValue.THREE),
+                new Card(CardSuit.HEARTS, CardValue.FOUR),
+                new Card(CardSuit.SPADES, CardValue.FOUR),
+                new Card(CardSuit.CLUBS, CardValue.FOUR),
+                new Card(CardSuit.CLUBS, CardValue.FIVE)
+        );
+
+        PokerHandMatcher matcher = new FourOfAKindMatcher();
+        assertFalse(matcher.matches(hand));
+    }
+
+    @Test
+    void FourOfAKindMatcherDoesNotMatchNonFourOfAKind2() {
+        PokerHand hand = new PokerHand(
+                new Card(CardSuit.DIAMONDS, CardValue.THREE),
+                new Card(CardSuit.HEARTS, CardValue.FOUR),
+                new Card(CardSuit.SPADES, CardValue.NINE),
+                new Card(CardSuit.CLUBS, CardValue.FOUR),
+                new Card(CardSuit.CLUBS, CardValue.FIVE)
+        );
+
+        PokerHandMatcher matcher = new FourOfAKindMatcher();
+        assertFalse(matcher.matches(hand));
+    }
+}
