422417e69c9d448c3c0510ceeefaddb8b83157c1
Patrick von Steht <4501324-patrick-vonsteht@users.noreply.gitlab.com>
2024-07-22 20:04:39 +0200

    Dry up and rename FlushMatcherTest

--- src/test/java/io/github/patrick_vonsteht/pokerhandkata/matchers/FlushMatcherTest.java
+++ src/test/java/io/github/patrick_vonsteht/pokerhandkata/matchers/FlushMatcherTest.java
@@ -6,34 +6,40 @@ import io.github.patrick_vonsteht.pokerhandkata.model.CardValue;
 import io.github.patrick_vonsteht.pokerhandkata.model.PokerHand;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FlushMatcherTest {
     @Test
-    void FlushMatcherMatchesFlush() {
-        PokerHand flushHand = new PokerHand(
-                new Card(CardSuit.CLUBS, CardValue.TWO),
-                new Card(CardSuit.CLUBS, CardValue.FOUR),
-                new Card(CardSuit.CLUBS, CardValue.JACK),
-                new Card(CardSuit.CLUBS, CardValue.FIVE),
-                new Card(CardSuit.CLUBS, CardValue.SIX)
-        );
-
-        FlushMatcher m = new FlushMatcher();
-        assertTrue(m.matches(flushHand));
+    void MatchesFlush() {
+        assertMatches(List.of(CardSuit.CLUBS, CardSuit.CLUBS, CardSuit.CLUBS, CardSuit.CLUBS, CardSuit.CLUBS));
     }
 
     @Test
-    void FlushMatcherDoesNotMatchNonFlush() {
-        PokerHand nonFlushHand = new PokerHand(
-                new Card(CardSuit.DIAMONDS, CardValue.TWO),
-                new Card(CardSuit.CLUBS, CardValue.FOUR),
-                new Card(CardSuit.CLUBS, CardValue.JACK),
-                new Card(CardSuit.CLUBS, CardValue.FIVE),
-                new Card(CardSuit.CLUBS, CardValue.SIX)
-        );
+    void DoesNotMatchNonFlush() {
+        assertDoesNotMatch(List.of(CardSuit.DIAMONDS, CardSuit.CLUBS, CardSuit.CLUBS, CardSuit.CLUBS, CardSuit.CLUBS));
+    }
 
+    private void assertMatches(List<CardSuit> handSuits) {
+        PokerHand hand = createHand(handSuits);
         FlushMatcher m = new FlushMatcher();
-        assertFalse(m.matches(nonFlushHand));
+        assertTrue(m.matches(hand));
+    }
+
+    private void assertDoesNotMatch(List<CardSuit> handSuits) {
+        PokerHand hand = createHand(handSuits);
+        FlushMatcher m = new FlushMatcher();
+        assertFalse(m.matches(hand));
+    }
+
+    private PokerHand createHand(List<CardSuit> handSuits) {
+        return new PokerHand(
+                new Card(handSuits.get(0), CardValue.TWO),
+                new Card(handSuits.get(1), CardValue.FOUR),
+                new Card(handSuits.get(2), CardValue.JACK),
+                new Card(handSuits.get(3), CardValue.FIVE),
+                new Card(handSuits.get(4), CardValue.SIX)
+        );
     }
 }
