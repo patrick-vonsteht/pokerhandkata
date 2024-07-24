72769eccf1744f3bc72807300729c7fb8d2b5f1e
Patrick von Steht <4501324-patrick-vonsteht@users.noreply.gitlab.com>
2024-07-24 18:42:29 +0200

    Remove duplicate test cases and dry up XOfAKindMatcherTest

--- src/test/java/io/github/patrick_vonsteht/pokerhandkata/matchers/XOfAKindMatcherTest.java
+++ src/test/java/io/github/patrick_vonsteht/pokerhandkata/matchers/XOfAKindMatcherTest.java
@@ -1,6 +1,7 @@
 package io.github.patrick_vonsteht.pokerhandkata.matchers;
 
 import io.github.patrick_vonsteht.pokerhandkata.PokerHandTestHelper;
+import io.github.patrick_vonsteht.pokerhandkata.TestValues;
 import io.github.patrick_vonsteht.pokerhandkata.model.CardValue;
 import io.github.patrick_vonsteht.pokerhandkata.model.PokerHand;
 import org.junit.jupiter.api.Test;
@@ -12,114 +13,51 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class XOfAKindMatcherTest {
 
+    Matcher oneThreeOfAKindMatcher = new XOfAKindMatcher(3, 1, 1);
+    Matcher oneOneOfAKindMatcher =new XOfAKindMatcher(1,1,1);
+    Matcher oneToFiveOneOfAKindMatcher = new XOfAKindMatcher(1, 1, 5);
+    Matcher oneFiveOfAKindMatcher = new XOfAKindMatcher(5, 1, 1);
+    Matcher twoTwoOfAKindMatcher =  new XOfAKindMatcher(2, 2, 2);
+
     @Test
-    void SingleOneOfAKindMatcherDoesNotMatchAnyHand() {
-        Matcher matcher = new XOfAKindMatcher(1, 1, 1);
-        assertDoesNotMatch(matcher, List.of(CardValue.TWO, CardValue.FOUR, CardValue.QUEEN, CardValue.ACE, CardValue.EIGHT));
+    void OneThreeOfAKindMatcherDoesNotMatchTwoOfAKind() {
+        assertDoesNotMatch(oneThreeOfAKindMatcher, TestValues.HIGH_PAIR_VALUES);
     }
 
     @Test
-    void MultipleOneOfAKindMatcherMatchesAnyHand() {
-        Matcher matcher = new XOfAKindMatcher(1, 1, 5);
-        assertMatches(matcher, List.of(CardValue.TWO, CardValue.FOUR, CardValue.QUEEN, CardValue.ACE, CardValue.EIGHT));
+    void OneThreeOfAKindMatcherMatchesThreeOfAKind() {
+        assertMatches(oneThreeOfAKindMatcher, TestValues.HIGH_THREE_OF_A_KIND_VALUES);
     }
 
     @Test
-    void SingleTwoOfAKindMatcherMatchesLowTwoOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(2, 1, 1);
-        assertMatches(matcher, List.of(CardValue.TWO, CardValue.FOUR, CardValue.TWO, CardValue.ACE, CardValue.EIGHT));
+    void OneThreeOfAKindMatcherDoesNotMatchFourOfAKind() {
+        assertDoesNotMatch(oneThreeOfAKindMatcher, TestValues.HIGH_FOUR_OF_A_KIND_VALUES);
     }
 
     @Test
-    void SingleTwoOfAKindMatcherMatchesMiddleTwoOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(2, 1, 1);
-        assertMatches(matcher, List.of(CardValue.TWO, CardValue.FOUR, CardValue.FOUR, CardValue.ACE, CardValue.EIGHT));
+    void OneOneOfAKindMatcherDoesNotMatchAnyHand() {
+        assertDoesNotMatch(oneOneOfAKindMatcher, TestValues.ANY_CARD_VALUES);
     }
 
     @Test
-    void SingleTwoOfAKindMatcherMatchesHighTwoOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(2, 1, 1);
-        assertMatches(matcher, List.of(CardValue.TWO, CardValue.FOUR, CardValue.ACE, CardValue.ACE, CardValue.EIGHT));
+    void OneToFiveOneOfAKindMatcherMatchesAnyHand() {
+        assertMatches(oneToFiveOneOfAKindMatcher, TestValues.ANY_CARD_VALUES);
     }
 
     @Test
-    void SingleTwoOfAKindMatcherDoesNotMatchNonTwoOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(2, 1, 1);
-        assertDoesNotMatch(matcher, List.of(CardValue.TEN, CardValue.FOUR, CardValue.TWO, CardValue.JACK, CardValue.QUEEN));
-    }
-
-    @Test
-    void SingleTwoOfAKindMatcherDoesNotMatchTwoTwoOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(2, 1, 1);
-        assertDoesNotMatch(matcher, List.of(CardValue.TEN, CardValue.FOUR, CardValue.TEN, CardValue.JACK, CardValue.JACK));
+    void OneFiveOfAKindMatcherMatchesFiveOfAKind() {
+        assertMatches(oneFiveOfAKindMatcher, TestValues.HIGH_FIVE_OF_A_KIND_VALUES);
     }
 
     @Test
     void TwoTwoOfAKindMatcherMatchesTwoTwoOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(2, 2, 2);
-        assertMatches(matcher, List.of(CardValue.TEN, CardValue.FOUR, CardValue.TEN, CardValue.JACK, CardValue.JACK));
+        assertMatches(twoTwoOfAKindMatcher, TestValues.TWO_PAIR_WITH_HIGH_FIRST_PAIR_VALUES);
     }
 
     @Test
     void TwoTwoOfAKindMatcherDoesNotMatchSingleTwoOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(2, 2, 2);
-        assertDoesNotMatch(matcher, List.of(CardValue.TEN, CardValue.FOUR, CardValue.TEN, CardValue.QUEEN, CardValue.JACK));
+        assertDoesNotMatch(twoTwoOfAKindMatcher, TestValues.HIGH_PAIR_VALUES);
     }
-
-    @Test
-    void ThreeOfAKindMatcherMatchesLowThreeOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(3, 1, 1);
-        assertMatches(matcher, List.of(CardValue.TWO, CardValue.FOUR, CardValue.TWO, CardValue.ACE, CardValue.TWO));
-    }
-
-    @Test
-    void ThreeOfAKindMatcherMatchesMiddleThreeOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(3, 1, 1);
-        assertMatches(matcher, List.of(CardValue.TWO, CardValue.FOUR, CardValue.FOUR, CardValue.ACE, CardValue.FOUR));
-    }
-
-    @Test
-    void ThreeOfAKindMatcherMatchesHighThreeOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(3, 1, 1);
-        assertMatches(matcher, List.of(CardValue.ACE, CardValue.FOUR, CardValue.ACE, CardValue.ACE, CardValue.EIGHT));
-    }
-
-    @Test
-    void ThreeOfAKindMatcherDoesNotMatchTwoOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(3, 1, 1);
-        assertDoesNotMatch(matcher, List.of(CardValue.FOUR, CardValue.FOUR, CardValue.TWO, CardValue.SIX, CardValue.QUEEN));
-    }
-
-    @Test
-    void FourOfAKindMatcherMatchesLowFourOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(4, 1, 1);
-        assertMatches(matcher, List.of(CardValue.TWO, CardValue.FOUR, CardValue.TWO, CardValue.TWO, CardValue.TWO));
-    }
-
-    @Test
-    void FourOfAKindMatcherMatchesHighFourOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(4, 1, 1);
-        assertMatches(matcher, List.of(CardValue.FOUR, CardValue.FOUR, CardValue.TWO, CardValue.FOUR, CardValue.FOUR));
-    }
-
-    @Test
-    void FourOfAKindMatcherDoesNotMatchThreeOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(4, 1, 1);
-        assertDoesNotMatch(matcher, List.of(CardValue.FOUR, CardValue.FOUR, CardValue.TWO, CardValue.FOUR, CardValue.QUEEN));
-    }
-
-    @Test
-    void FiveOfAKindMatcherMatchesFiveOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(5, 1, 1);
-        assertMatches(matcher, List.of(CardValue.THREE, CardValue.THREE, CardValue.THREE, CardValue.THREE, CardValue.THREE));
-    }
-
-    @Test
-    void FiveOfAKindMatcherDoesNotMatchFourOfAKind() {
-        Matcher matcher = new XOfAKindMatcher(5, 1, 1);
-        assertDoesNotMatch(matcher, List.of(CardValue.FOUR, CardValue.FOUR, CardValue.FOUR, CardValue.FOUR, CardValue.QUEEN));
-    }
-
 
     private void assertMatches(Matcher matcher, List<CardValue> handValues) {
         PokerHand hand = PokerHandTestHelper.createHandFromValues(handValues);
